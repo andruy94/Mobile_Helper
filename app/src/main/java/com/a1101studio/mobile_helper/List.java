6ae6b5b39d4b07196163bf57355697914eafb2bc
Даниил <Sofon000@ya.ru>
2016-12-17 21:19:50 +0300

    1

---
+++ app/src/main/java/com/a1101studio/mobile_helper/List.java
@@ -0,0 +1,13 @@
+package com.a1101studio.mobile_helper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}

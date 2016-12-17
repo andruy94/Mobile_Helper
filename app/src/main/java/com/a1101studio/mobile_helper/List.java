a0163b4c0a379fa381738ef32d756a8776d1d800
andruy94 <andruy94@ya.ru>
2016-12-17 21:54:01 +0300

    список

--- app/src/main/java/com/a1101studio/mobile_helper/List.java
+++ app/src/main/java/com/a1101studio/mobile_helper/List.java
@@ -2,12 +2,21 @@ package com.a1101studio.mobile_helper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class List extends AppCompatActivity {
+    private ListView listView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        listView = (ListView) findViewById(R.id.lv1);
+        String[] strings={"1","2","3"};//тут пишешь что надо
+        ArrayAdapter arrayAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,strings);
+        listView.setAdapter(arrayAdapter);
     }
 }

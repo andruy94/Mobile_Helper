5de28f5de224f185de3f6b031906c1c21b59c27d
andruy94 <andruy94@ya.ru>
2017-02-08 21:37:27 +0300

    Правки в адаптере к списку

--- app/src/main/java/com/a1101studio/mobile_helper/adapters/ListAdapter.java
+++ app/src/main/java/com/a1101studio/mobile_helper/adapters/ListAdapter.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 
 import com.a1101studio.mobile_helper.R;
 import com.a1101studio.mobile_helper.models.CheckListItem;
+import com.a1101studio.mobile_helper.models.DefectCheckListItem;
 import com.a1101studio.mobile_helper.singleton.WorkData;
 
 /**
@@ -23,49 +24,50 @@ public class ListAdapter extends ArrayAdapter<CheckListItem> {
    // private int checkBoxCount=3;
     private final Context context;
 
-    private final CheckListItem[] checkListItem;
+    private final CheckListItem[] checkListItems;
     public ListAdapter(Context context, CheckListItem[] checkListItem, int k) {
         super(context, R.layout.list_item, checkListItem);
         this.context=context;
-        this.checkListItem=WorkData.getInstance().getCheckListItemList().get(k);//checkListItem;
+        this.checkListItems=WorkData.getInstance().getCheckListItemList().get(k);//checkListItem;
     }
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        CheckListItem checkListItem=checkListItems[position];
         final LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         final View rowView = inflater.inflate(R.layout.list_item, parent, false);
 
         CheckBox titleCheckBox=(CheckBox) rowView.findViewById(R.id.chbHeader);
-        titleCheckBox.setChecked(checkListItem[position].isChecked());
+        titleCheckBox.setChecked(checkListItems[position].isChecked());
 
-        final CheckBox[] checkBoxes=new CheckBox[checkListItem[position].getCheckListItems().length];
-        for(int i=0;i<checkListItem[position].getCheckListItems().length;i++){
+        /*final CheckBox[] checkBoxes=new CheckBox[checkListItem.getDefectCheckListItems().length];
+        for(int i=0;i<checkListItem.getDefectCheckListItems().length;i++){
             checkBoxes[i] = new CheckBox(context);
-            checkBoxes[i].setText(checkListItem[position].getCheckListItems()[i].getDescription());
-            checkBoxes[i].setChecked(checkListItem[position].getCheckListItems()[i].isChecked());
+            checkBoxes[i].setText(checkListItem.getDefectCheckListItems()[i].getCheckBoxItem().getTitle());
+            checkBoxes[i].setChecked(checkListItem.getDefectCheckListItems()[i].getCheckBoxItem().isChecked());
             int[] k={i};
             checkBoxes[i].setOnClickListener(v -> {
-                checkListItem[position].getCheckListItems()[k[0]].setChecked( !checkListItem[position].getCheckListItems()[k[0]].isChecked());
-                checkListItem[position].setChecked(checkListItem[position].getCheckListItems()[k[0]].isChecked());
+                DefectCheckListItem defectCheckListItem=checkListItem.getDefectCheckListItems()[k[0]];
+                defectCheckListItem.getCheckBoxItem().setChecked( !defectCheckListItem.getCheckBoxItem().isChecked());
+                checkListItem.setChecked(defectCheckListItem.getCheckBoxItem().isChecked());
                 titleCheckBox.setChecked(true);
 
             });
-
-        }
+        }*/
 
 
         TextView textView = (TextView) rowView.findViewById(R.id.text1);
-        textView.setText(checkListItem[position].getDescription());
+        textView.setText(checkListItem.getDescription());
 
         final boolean[] flag = {true};
         textView.setOnClickListener(v -> {
             if(flag[0]) {
                 LinearLayout linearLayout = (LinearLayout) rowView.findViewById(R.id.ll_second_list);
                 // View child=inflater.inflate(R.layout.inner_item,linearLayout);
-                for(int i=0;i<checkListItem[position].getCheckListItems().length;i++) {
+                /*for(int i=0;i<checkListItem.getDefectCheckListItems().length;i++) {
                     linearLayout.addView(checkBoxes[i]);
-                    checkBoxes[i].setChecked(checkListItem[position].getCheckListItems()[i].isChecked());
-                }
+                    checkBoxes[i].setChecked(checkListItem.getDefectCheckListItems()[i].getCheckBoxItem().isChecked());
+                }*/
                 flag[0] =!flag[0];
             }else {
                 LinearLayout linearLayout = (LinearLayout) rowView.findViewById(R.id.ll_second_list);

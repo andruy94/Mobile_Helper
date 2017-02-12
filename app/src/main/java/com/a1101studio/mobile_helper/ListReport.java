a9113f2686ae1cd097d4e0f26eb9b2a52ccf73fe
Даниил <Sofon000@ya.ru>
2017-02-12 21:23:10 +0300

    Лист

---
+++ app/src/main/java/com/a1101studio/mobile_helper/ListReport.java
@@ -0,0 +1,30 @@
+package com.a1101studio.mobile_helper;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ListView;
+
+public class ListReport extends AppCompatActivity {
+    ListView listView = (ListView)findViewById(R.id.listView);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_report);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}

c334331dc70d7bb0c1cf793fba0608907daf1848
Даниил <Sofon000@ya.ru>
2017-02-13 22:25:42 +0300

    Файлики

--- app/src/main/java/com/a1101studio/mobile_helper/ListReport.java
+++ app/src/main/java/com/a1101studio/mobile_helper/ListReport.java
@@ -6,17 +6,31 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class ListReport extends AppCompatActivity {
-    ListView listView = (ListView)findViewById(R.id.listView);
+    ListView listView = (ListView) findViewById(R.id.listView);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_report);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        File dir = new File("/");
+        File[] filelist = dir.listFiles();
+        String[] theNamesOfFiles = new String[filelist.length];
+        for (int i = 0; i < theNamesOfFiles.length; i++) {
+            theNamesOfFiles[i] = filelist[i].getName();
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, theNamesOfFiles);
 
+// используем адаптер данных
+        listView.setAdapter(adapter);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +39,7 @@ public class ListReport extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
 
+
+    }
 }
